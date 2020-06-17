8314c252f8b56a936d7b7e0a0ea9f0100f50714b
Benjamin Lellouch <blellouch@gmail.com>
2020-06-17 16:22:57 +0100

    added ability to delete pastes

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,26 @@
 mod paste_id;
 
 use paste_id::PasteId;
-use std::io;
 use std::path::Path;
 use std::fs::File;
 use std::fs;
 use std::io::prelude::*;
 
-use rocket::Data;
-use rocket::http::RawStr;
+
 use rocket::request::Form;
 use rocket_contrib::templates::Template;
+use rocket::response::{Redirect, Flash};
 
 #[derive(FromForm)]
 struct Paste
 {
     content: String
+}
+
+#[derive(FromForm)]
+struct DeletePaste
+{
+    pastes: String
 }
 
 #[derive(Serialize)]
@@ -54,16 +59,22 @@ fn read_dir(path: &Path) -> Vec<String>
 }
 
 #[post("/", data = "<paste>")]
-fn upload(paste: Form<Paste>) -> Result<String, std::io::Error>
+fn upload(paste: Form<Paste>) -> Result<Flash<Redirect>, std::io::Error>
 {
     let id = PasteId::new(3);
     let filename = format!("upload/{id}", id = id);
-    let url = format!("{host}/{id}\n", host = "http://localhost:8000", id = id);
 
     let mut file = File::create(&filename)?;
     file.write(paste.content.as_bytes())?;
 
-    Ok(url)
+    Ok(Flash::success(Redirect::to("/"), "Successfully created paste"))
+}
+
+#[post("/delete", data="<paste>")]
+fn delete(paste: Form<DeletePaste>) -> Result<Flash<Redirect>, std::io::Error>
+{
+    fs::remove_file(format!("upload/{id}", id = paste.pastes))?;
+    Ok(Flash::success(Redirect::to("/"), "Successfully deleted paste"))
 }
 
 #[get("/<id>")]
@@ -75,7 +86,7 @@ fn retrieve(id: PasteId) -> Option<File>
 
 fn main() {
     rocket::ignite()
-    .mount("/", routes![index, upload, retrieve])
+    .mount("/", routes![index, upload, retrieve, delete])
     .attach(Template::fairing())
     .launch();
 }
